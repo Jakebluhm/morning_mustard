8aedeb7bc3cd8c922707c78a7e5cd9c2a386cba5
unknown <bluhmj96@gmail.com>
2023-12-31 14:55:06 -0500

    Thoughts page

--- lib/pages/thoughts_page.dart
+++ lib/pages/thoughts_page.dart
@@ -1,28 +1,22 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_hooks/flutter_hooks.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 import 'package:morning_mustard/services/navigation_service/navigation_service.dart';
 
 class ThoughtsPage extends HookConsumerWidget {
   ThoughtsPage();
-//          lib/assets/Thought/chain.png
-//          lib/assets/Thought/fact.png
-//          lib/assets/Thought/Feelings.png
-//          lib/assets/Thought/I Take my Thoughts  Captive to the  Obedience of Jesus.png
-//          lib/assets/Thought/Jesus.png
-//          lib/assets/Thought/Shape 2.png
-//          lib/assets/Thought/t1.png
-//          lib/assets/Thought/t2.png
-//          lib/assets/Thought/t3.png
-//          lib/assets/Thought/Thoughts.png
+
   @override
   Widget build(BuildContext context, WidgetRef ref) {
+    final textState = useState<String>('');
     double screenWidth = MediaQuery.of(context).size.width;
     double screenHeight = MediaQuery.of(context).size.height;
 
     double imageHeight = screenHeight * 0.8;
+    double imageWidth = imageHeight * 0.7747;
     return Scaffold(
       appBar: AppBar(
-        title: Text('Armor'),
+        title: Text('Thoughts'),
         backgroundColor: Color.fromRGBO(255, 205, 88, 1),
         leading: IconButton(
           icon: Icon(Icons.arrow_back),
@@ -46,12 +40,45 @@ class ThoughtsPage extends HookConsumerWidget {
         child: Stack(
           alignment: Alignment.center,
           children: [
-            Align(
-              alignment: Alignment.center,
-              child: Image.asset(
-                'lib/assets/Thought/chain.png',
-                fit: BoxFit.contain,
-                height: imageHeight,
+            Transform.translate(
+              offset: Offset(0, screenHeight * -0.13),
+              child: Container(
+                width: imageWidth,
+                child: Padding(
+                  padding: const EdgeInsets.all(4.0),
+                  child: Image.asset(
+                    'lib/assets/Thought/Jesus.png',
+                    fit: BoxFit.contain,
+                  ),
+                ),
+              ),
+            ),
+            Transform.translate(
+              offset: Offset(0, screenHeight * -0.15),
+              child: Container(
+                width: imageWidth,
+                child: Padding(
+                  padding: EdgeInsets.only(
+                      right: screenWidth * .15, left: screenWidth * .15),
+                  child: TextField(
+                    onChanged: (newText) {
+                      textState.value = newText; // Updating the state
+                    },
+                    decoration: InputDecoration(
+                      border: OutlineInputBorder(),
+                    ),
+                  ),
+                ),
+              ),
+            ),
+            IgnorePointer(
+              child: Align(
+                alignment: Alignment.center,
+                child: Image.asset(
+                  'lib/assets/Thought/chain.png',
+                  fit: BoxFit.contain,
+                  height: imageHeight,
+                ),
               ),
             ),
             Transform.translate(
@@ -59,9 +86,9 @@ class ThoughtsPage extends HookConsumerWidget {
               child: Container(
                 decoration: BoxDecoration(
                     color: Color.fromRGBO(17, 24, 102, 1),
-                    border: Border.all()),
+                    borderRadius: BorderRadius.circular(10)),
                 height: screenHeight * 0.15,
-                width: screenWidth,
+                width: imageWidth,
                 child: Padding(
                   padding: const EdgeInsets.all(4.0),
                   child: Image.asset(
@@ -77,9 +104,9 @@ class ThoughtsPage extends HookConsumerWidget {
               child: Container(
                   decoration: BoxDecoration(
                       color: const Color.fromRGBO(17, 24, 102, 1),
-                      border: Border.all()),
+                      borderRadius: BorderRadius.circular(10)),
                   height: screenHeight * 0.05,
-                  width: screenWidth,
+                  width: imageWidth,
                   child: Container()),
             ),
             // Words
@@ -97,7 +124,7 @@ class ThoughtsPage extends HookConsumerWidget {
               ),
             ),
             Transform.translate(
-              offset: Offset(screenWidth * 0.20, screenHeight * 0.3),
+              offset: Offset(screenWidth * 0.27, screenHeight * 0.3),
               child: Container(
                 height: screenHeight * 0.05,
                 child: Padding(
@@ -110,7 +137,7 @@ class ThoughtsPage extends HookConsumerWidget {
               ),
             ),
             Transform.translate(
-              offset: Offset(screenWidth * -0.20, screenHeight * 0.3),
+              offset: Offset(screenWidth * -0.27, screenHeight * 0.3),
               child: Container(
                 height: screenHeight * 0.05,
                 child: Padding(
@@ -124,9 +151,9 @@ class ThoughtsPage extends HookConsumerWidget {
             ),
             // Trains
             Transform.translate(
-              offset: Offset(0, screenHeight * 0.225),
+              offset: Offset(screenWidth * -0.27, screenHeight * 0.225),
               child: Container(
-                height: screenHeight * 0.1,
+                height: screenHeight * 0.11,
                 child: Padding(
                   padding: const EdgeInsets.all(4.0),
                   child: Image.asset(
@@ -137,9 +164,9 @@ class ThoughtsPage extends HookConsumerWidget {
               ),
             ),
             Transform.translate(
-              offset: Offset(screenWidth * 0.20, screenHeight * 0.22),
+              offset: Offset(0, screenHeight * 0.225),
               child: Container(
-                height: screenHeight * 0.1,
+                height: screenHeight * 0.11,
                 child: Padding(
                   padding: const EdgeInsets.all(4.0),
                   child: Image.asset(
@@ -150,9 +177,9 @@ class ThoughtsPage extends HookConsumerWidget {
               ),
             ),
             Transform.translate(
-              offset: Offset(screenWidth * -0.20, screenHeight * 0.22),
+              offset: Offset(screenWidth * 0.27, screenHeight * 0.225),
               child: Container(
-                height: screenHeight * 0.1,
+                height: screenHeight * 0.11,
                 child: Padding(
                   padding: const EdgeInsets.all(4.0),
                   child: Image.asset(
